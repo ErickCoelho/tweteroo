102668ca6ce0d0e12c9a6d368497ac5c0239ce79
Erick Coelho <erickcoelho06@gmail.com>
2023-09-18 09:18:45 -0300

    sign up and tweet data validation

--- api.js
+++ api.js
@@ -9,6 +9,11 @@ const users = [];
 const tweets = [];
 
 app.post('/sign-up', (req, res) => {
+    if(req.body.username === "" || req.body.avatar === ""){
+        res.status(400).send("Todos os campos devem estar preenchidos!");
+        return;
+    }
+
     const user = req.body;
     users.push(user);
     console.log(users);
@@ -16,6 +21,11 @@ app.post('/sign-up', (req, res) => {
 });
 
 app.post('/tweets', (req, res) => {
+    if(req.body.tweet === ""){
+        res.status(400).send('O campo tweet não pode ser vazio!');
+        return;
+    }
+
     const postUser = users.find((users) => users.username === req.body.username);
     const tweet = {...req.body, avatar: postUser.avatar};
     tweets.push(tweet);
